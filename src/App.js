a2d9bc42defd220683d28c981d31d71ff1ff155d
“Foxshane997” <Foxshane997@gmail.com>
2024-07-13 12:12:27 -0700

    added a test component

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
+import Boiler from "./components/boiler";
 
 function App() {
   return (
  <>
  <h1>Recipe App</h1>
+ <Boiler/>
  </>
   );
 }
---
+++ src/components/boiler.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+function Boiler() {
+    return (
+        <div>
+        <h1>
+            Boiler Component
+        </h1>
+        <p>
+            this is a test Component.
+        </p>
+        </div>
+    )
+}
+
+export default Boiler
